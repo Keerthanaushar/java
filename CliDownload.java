d6aabfb230a68e5705e0307d9aa8537d42d5fdac
Keerthanaushar <120699074+Keerthanaushar@users.noreply.github.com>
2023-03-23 12:02:32 +0530

    Add files via upload

---
+++ CliDownload.java
@@ -0,0 +1,28 @@
+package Networking;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class CliDownload {
+
+	public static void main(String[] args) {
+		try
+		{
+			Socket s=new Socket("localhost",1234);
+			System.out.println("Sending a request to server");
+			DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+			DataInputStream din=new DataInputStream(System.in);
+			System.out.println("Enter data u want to send");
+			String st=din.readLine();
+			dout.writeUTF(st);
+			s.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+
+	}
+
+}
